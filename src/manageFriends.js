c6b6e0f709ab5cb4ebe1e7ccb975839b4ac29215
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-09-27 10:05:53 -0400

    commit tree d88f1a4dc8a1d874e25347fd1404e66d55f2246a

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
 export function manageFriends(state, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add" :
+      return {friends: [...state.friends,{
+        name: action.payload.name,
+        hometown: action.payload.hometown,
+        id: action.payload.id
+      }]}
+    case "friends/remove" :
+      return {friends: state.friends.filter(friend=>friend.id!==action.payload)}
+    default :
+      return JSON.parse(JSON.stringify(state))
+  }
 }
